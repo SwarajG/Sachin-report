edd6136c5ba68ebf68d732814f85d6ada4c7553b
SwarajG <gandhiswaraj9067008148@gmail.com>
2017-04-15 01:17:36 +0530

    Added port to node

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Basic structure for running the app
 
 'use strict';
+const port = parseInt(process.env.PORT, 10) || 3000;
 const express = require('express');
 const routes =  require('./backend/routes');
 
@@ -9,6 +10,4 @@ let app = express();
 app.use(express.static('public'));
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Your app is running on 3000 port')
-})
+app.listen(port);
